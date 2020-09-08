a67d5b0bab3de56f179a47d256ae583321b6b689
deloittesentimentpoc11 <67398619+deloittesentimentpoc11@users.noreply.github.com>
2020-09-08 14:30:58 -0400

    Update step1_environment_variables.sh

--- step1_environment_variables.sh
+++ step1_environment_variables.sh
@@ -1,7 +1,27 @@
-echo "# Python_Code" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git remote add origin https://github.com/Deloitte-GCP-USDC-Developers/Python_Code.git
-git push -u origin master
-                
+#!/bin/sh
+
+##Set Region
+export REGION=us-east1
+
+##FILES_SOURCE is set up as an environment variable
+export FILES_SOURCE=tidal-glider-217500-files-source-1586544872
+
+##FUNCTIONS_BUCKET to stage functions during deployment is set up as an environment variable
+export FUNCTIONS_BUCKET=tidal-glider-217500-functions-1586545717
+
+##Provision a Pub/Sub topic, called 'streaming_error_topic', to handle the error path.
+export STREAMING_ERROR_TOPIC=streaming_error_topic
+
+##Provision a Pub/Sub topic, called 'streaming_success_topic', to handle the success path.
+export STREAMING_SUCCESS_TOPIC=streaming_success_topic
+
+##Handle streaming errors
+##Create your Cloud Storage bucket to store problematic files.
+##FILES_ERROR is set up as an environment variable with a unique name for the bucket
+##that stores error files.
+export FILES_ERROR=tidal-glider-217500-files-error-1586547274
+
+##Handle successful streaming
+##Create your Coldline Cloud Storage bucket. FILES_SUCCESSis set up as an environment
+##variable with a unique name for the bucket that stores success files.
+export FILES_SUCCESS=tidal-glider-217500-files-success-1586547731
