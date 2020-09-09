70a68d50f0df270f33a1fb18c1c0f41bd2fc8791
deloittesentimentpoc11 <67398619+deloittesentimentpoc11@users.noreply.github.com>
2020-09-08 21:34:39 -0400

    Update step1_environment_variables.sh

--- step1_environment_variables.sh
+++ step1_environment_variables.sh
@@ -4,10 +4,10 @@
 export REGION=us-east1
 
 ##FILES_SOURCE is set up as an environment variable
-export FILES_SOURCE=tidal-glider-217500-files-source-1586544872
+export FILES_SOURCE=dlt-sntmnt-poc-284722-files-source-1599614649
 
 ##FUNCTIONS_BUCKET to stage functions during deployment is set up as an environment variable
-export FUNCTIONS_BUCKET=tidal-glider-217500-functions-1586545717
+export FUNCTIONS_BUCKET=dlt-sntmnt-poc-284722-functions-1599614727
 
 ##Provision a Pub/Sub topic, called 'streaming_error_topic', to handle the error path.
 export STREAMING_ERROR_TOPIC=streaming_error_topic
@@ -19,9 +19,9 @@ export STREAMING_SUCCESS_TOPIC=streaming_success_topic
 ##Create your Cloud Storage bucket to store problematic files.
 ##FILES_ERROR is set up as an environment variable with a unique name for the bucket
 ##that stores error files.
-export FILES_ERROR=tidal-glider-217500-files-error-1586547274
+export FILES_ERROR=dlt-sntmnt-poc-284722-files-error-1599615079
 
 ##Handle successful streaming
 ##Create your Coldline Cloud Storage bucket. FILES_SUCCESSis set up as an environment
 ##variable with a unique name for the bucket that stores success files.
-export FILES_SUCCESS=tidal-glider-217500-files-success-1586547731
+export FILES_SUCCESS=dlt-sntmnt-poc-284722-files-success-1599615235
